c24daf9058e700b1918c8e5953787b63806609e2
Am1d1v <dimadiman1adima@gmail.com>
2024-02-01 14:16:43 +0300

    Play and Pause Song Functions

--- script.js
+++ script.js
@@ -27,6 +27,44 @@ loadSong(songs[songIndex]);
 // Update song details
 function loadSong(song){
     title.innerText = song;
-    audio.src = `music/${song}.mp3`;
+    audio.src = `audio/${song}.mp3`;
     cover.src = `images/${song}.jpg`
 }
+
+// Play Song
+function playSong(){
+    musicContainer.classList.add('play');
+
+    // Change Play Button to Stop Button
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+// Pause Song
+function pauseSong(){
+    musicContainer.classList.remove('play');
+
+    // Change Play Button to Stop Button
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+
+    audio.pause();
+}
+
+
+// Event Listeners
+
+// Play Button
+playBtn.addEventListener('click', () => {
+
+    // Check playing song status
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying){
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
